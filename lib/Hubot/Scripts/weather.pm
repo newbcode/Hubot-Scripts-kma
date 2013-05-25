03fd1de4ab694713ed584e70f79113cff0b0092a
newbcode <newbcode@newb-dtop>
2013-05-25 16:51:49 +0900

    지역 연속 검색 추가

--- lib/Hubot/Scripts/weather.pm
+++ lib/Hubot/Scripts/weather.pm
@@ -23,7 +23,8 @@ sub load {
     my ( $class, $robot ) = @_;
  
     $robot->hear(
-        qr/^weather weekly (.*)/i,    
+        #qr/^weather weekly (.*)/i,    
+        qr/^w (.*)/i,    
         \&parser_process,
     );
 }
@@ -31,72 +32,72 @@ sub load {
 sub parser_process {
     my $msg = shift;
 
+    my $count = 0;
+    my $table;
     my $user_input = $msg->match->[0];
     my @citynames = split (/ /, $user_input );
     my @country_numbers;
-    #$msg->send("@citynames");
+
     for my $country ( keys %countris ) {
         for my $cityname ( @citynames ) {
             if ( $countris{$country} =~ /$cityname/ ) {
-                $country_number = $country;
-                $msg->send("$country_number");
-                push @country_numbers, $country_number;
+            $msg->http("http://www.kma.go.kr/weather/forecast/mid-term_$country.jsp")->get(
+                    sub {
+                        my ( $body, $hdr ) = @_;
+                        $msg->send('in sub callback');
+
+                        return if ( !$body || $hdr->{Status} !~ /^2/ );
+
+                        my $announcementtime;
+                        my $decode_body = decode("euc-kr", $body);
+                        if ( $decode_body =~ m{<p class="mid_announcementtime fr">.*?<span>(.*?)</span></p>} ) {
+                            $announcementtime = $1;
+                        }
+
+                        #
+                        # 날씨 정보를 해시화
+                        # @temperatures 변수를 만든 후 모두 소모함
+                        #
+                        my @cities = $decode_body =~ m{<th scope="row">(.*?)</th>}gsm;
+                        my @days   = $decode_body =~ m{<th scope="col"  class="top_line" style=".*?">(.*?)</th>}gsm; 
+                        my @temperatures;
+                        while ( $decode_body =~ m{<li><span class="col_blue">(\d+)</span> / <span class="col_orange">(\d+)</span></li>}gms ) {
+                            push @temperatures, "$1/$2";
+                        }
+                        #
+                        #$weather($city)가 첫번쨰 불릴때는 당연히 FALSE인데 초기값을 
+                        #arrayref 넣고 다음부터 참조될때는 arrayref니까 @{}로 보간된 ref를 풀고 push를 함
+                        #
+                        my %weather;
+                        for my $city (@cities) {
+                            for ( 1 .. @days ) {
+                                push @{ $weather{$city} ||= [] }, shift(@temperatures);
+                            }
+                        }
+                        #
+                        # show table
+                        #
+                        if ( $count == 0 ) {
+                                $table = Text::ASCIITable->new({
+                                utf8        => 0,
+                                headingText => "최저/최고기온(℃ )[$announcementtime]",
+                                cb_count    => sub { mbswidth(shift) },
+                            });
+                        $table->setCols( "도시", @days );
+                        }
+                        $count++;
+                        for my $city (keys %weather) {
+                            next unless $cityname eq $city;
+
+                            $table->addRow( $city, @{ $weather{$city}} );
+                        }
+                        if ($count == $#citynames + 1) {
+                            $msg->send("\n"), $msg->send($table);
+                        }
+                    }
+                );
             }
         }
-    }
-    for my $page_num ( @country_numbers ) {
-        $msg->http("http://www.kma.go.kr/weather/forecast/mid-term_$page_num.jsp")->get(
-        sub {
-            my ( $body, $hdr ) = @_;
-
-            return if ( !$body || $hdr->{Status} !~ /^2/ );
-
-            my $announcementtime;
-            my $decode_body = decode("euc-kr", $body);
-            if ( $decode_body =~ m{<p class="mid_announcementtime fr">.*?<span>(.*?)</span></p>} ) {
-                $announcementtime = $1;
-            }
-
-            #
-            # 날씨 정보를 해시화
-            # @temperatures 변수를 만든 후 모두 소모함
-            #
-            my @cities = $decode_body =~ m{<th scope="row">(.*?)</th>}gsm;
-            my @days   = $decode_body =~ m{<th scope="col"  class="top_line" style=".*?">(.*?)</th>}gsm; 
-            my @temperatures;
-            while ( $decode_body =~ m{<li><span class="col_blue">(\d+)</span> / <span class="col_orange">(\d+)</span></li>}gms ) {
-                push @temperatures, "$1/$2";
-            }
-            #
-            #$weather($city)가 첫번쨰 불릴때는 당연히 FALSE인데 초기값을 
-            #arrayref 넣고 다음부터 참조될때는 arrayref니까 @{}로 보간된 ref를 풀고 push를 함
-            #
-            my %weather;
-            for my $city (@cities) {
-                for ( 1 .. @days ) {
-                    push @{ $weather{$city} ||= [] }, shift(@temperatures);
-                }
-            }
-
-            #
-            # show table
-            #
-            my $table = Text::ASCIITable->new({
-                utf8        => 0,
-                headingText => "최저/최고기온(℃ )[$announcementtime]",
-                cb_count    => sub { mbswidth(shift) },
-            });
-
-            $table->setCols( "도시", @days );
-            for my $city (keys %weather) {
-                next unless $user_input eq $city;
-
-                $table->addRow( $city, @{ $weather{$city}} );
-            }
-            $msg->send("\n");
-            $msg->send("$table");
-        }
-        );
     }
 }
  
